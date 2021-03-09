c39d27797021ead5aca95a6dfe08d72c32542f2f
naveenkumarkl <naveen3031992@gmail.com>
2021-03-09 12:06:19 +0530

    first deployment

---
+++ src/components/footer.php
@@ -0,0 +1,80 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+
+    <!--google fonts-->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- css link -->
+    <link rel="stylesheet" href="./svce/css/style.css">
+
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="footers">
+        <div class="container py-5">
+            <div class="row">
+                <div class="col-md-5 col-sm-12">
+                    <div class="col1">
+                        <h5>Logo</h5>
+                    </div>
+                    <div class="col1 ">
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum modi alias, repellendus amet dolorem voluptatibus! Esse at, fugit voluptatum ut vel sit expedita voluptatem atque mollitia tempora, unde impedit consequatur?</p>
+                    </div>
+                </div>
+                <div class=" col-md-3 px-lg-5 col-sm-12">
+                    <div class="col1 ">
+                        <h5>More Links</h5>
+                    </div>
+                    <div class="col1 ">
+                        <div class="list-group">
+                            <a href="#" class="list-group-item list-group-item-action">
+                                Home
+                            </a>
+                            <a href=" #" class="list-group-item list-group-item-action">About us</a>
+                            <a href="#" class="list-group-item list-group-item-action">Blog</a>
+                            <a href="#" class="list-group-item list-group-item-action">Contact us</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4 px-lg-5 col-sm-12">
+
+
+                    <div class="col1 ">
+                        <h5>Social Links</h5>
+                    </div>
+                    <div class="col1">
+                        <i class="bi bi-linkedin"></i>
+                        <i class="bi bi-facebook"></i>
+                        <i class="bi bi-twitter"></i>
+                        <i class="bi bi-instagram"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="copyright p-2">
+        <span><a></a>&copy;<script>
+                document.write(new Date().getFullYear())
+            </script></a><a> Company Name </a>/ <a href="#">Privacy Policy</a> / <a href="#">Terms & Conditions</a></span>
+    </div>
+
+
+    <!-- Option : Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.min.js" integrity="sha384-nsg8ua9HAw1y0W1btsyWgBklPnCUAFLuTMS2G72MMONqmOymq585AcH49TLBQObG" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
