96f4e7b679f9d99c64b1946192184ecb72a807d1
naveenkumarkl <naveen3031992@gmail.com>
2021-03-11 11:12:58 +0530

    about us2

--- about_us.php
+++ about_us.php
@@ -4,7 +4,7 @@
     <div class="row">
         <div class="col-4">
             <div class="card" style="width: 18rem;">
-                <img src="https://images.unsplash.com/photo-1423530990185-af7dde1155fb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1109&q=80" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1423530990185-af7dde1155fb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1109&q=80" class="card-img-top lazy" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -14,7 +14,7 @@
         </div>
         <div class="col-4">
             <div class="card" style="width: 18rem;">
-                <img src="https://images.unsplash.com/photo-1454789591675-556c287e39e2?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top" alt="...">
+                <img src="https://images.unsplash.com/photo-1454789591675-556c287e39e2?ixid=MXwxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8NnNNVmpUTFNrZVF8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60" class="card-img-top lazy" alt="...">
                 <div class="card-body">
                     <h5 class="card-title">Card title</h5>
                     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
@@ -35,7 +35,7 @@
     </div>
     <div class="row my-5">
         <div class="col-4">
-            <img src="https://images.unsplash.com/photo-1459909633680-206dc5c67abb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" class="img-thumbnail" alt="...">
+            <img src="https://images.unsplash.com/photo-1459909633680-206dc5c67abb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" class="img-thumbnail lazy" alt="...">
         </div>
         <div class="col-8">
             <h1>How to add Preloader on your website using CSS & jQuery</h1>
@@ -49,7 +49,7 @@
             <p>In this article, we going to see how to add Preloader on your website using CSS & jQuery. Preloaders are most important to the websites, this will help to stay the user on our website instead of jumping to another website. You can use the preloader in two ways one is using gif image and another with CSS loader, using any one of this method you can implement the loader on your website. Here we going to use gif image, this will hide once all website images, content and scripts loaded successfully.</p>
         </div>
         <div class="col-4">
-            <img src="https://images.unsplash.com/photo-1615271292858-7107dcb37363?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80" class="img-thumbnail" alt="...">
+            <img src="https://images.unsplash.com/photo-1459909633680-206dc5c67abb?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80" class="img-thumbnail lazy" alt="...">
         </div>
 
     </div>
