5c1b38e48d2c28f26e5fa838c992928bfbe3f6ee
naveenkumarkl <naveen3031992@gmail.com>
2021-03-17 11:10:00 +0530

    testgzip

--- header.php
+++ header.php
@@ -16,6 +16,8 @@ else ob_start();
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" media="print" onload="this.media='all'" crossorigin> -->
     <link rel="preload" href="./css/bootstrap/css/bootstrap.min.css" as="style">
     <link rel="stylesheet" href="./css/bootstrap/css/bootstrap.min.css">
+
+    <link rel="preload" href="./css/bootstrap/js/bootstrap.min.js" as="script">
     <!--google fonts-->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
