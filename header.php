fbfefa5e3dd98dbf9a0dc5b5c55f90464cbea72b
naveenkumarkl <naveen3031992@gmail.com>
2021-03-17 11:55:10 +0530

    testgzip

--- header.php
+++ header.php
@@ -11,12 +11,9 @@ else ob_start();
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- Bootstrap icon -->
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" media="print" onload="this.media='all'" crossorigin> -->
     <!-- Bootstrap CSS -->
-
-
-
-
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" media="print" onload="this.media='all'" crossorigin>
     <link rel="preload" href="./css/bootstrap/css/bootstrap.min.css" as="style">
     <link rel="stylesheet" href="./css/bootstrap/css/bootstrap.min.css">
     <link rel="preload" href="./css/bootstrap/js/bootstrap.min.js" as="script">
