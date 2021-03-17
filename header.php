9ee4dda28912f3da975377917d4346117980443e
naveenkumarkl <naveen3031992@gmail.com>
2021-03-17 11:42:09 +0530

    testgzip

--- header.php
+++ header.php
@@ -12,13 +12,17 @@ else ob_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
+    <link rel="preload" as="style" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" onload="this.rel='stylesheet'" />
+    <link rel="preload" as="font" type="font/woff2" crossorigin href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" />
+
+
+
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" media="print" onload="this.media='all'" crossorigin> -->
     <link rel="preload" href="./css/bootstrap/css/bootstrap.min.css" as="style">
     <link rel="stylesheet" href="./css/bootstrap/css/bootstrap.min.css">
-
     <link rel="preload" href="./css/bootstrap/js/bootstrap.min.js" as="script">
     <!--google fonts-->
-    <!-- <link rel="preload" href="./css/font.css" as="style"> -->
+    <link rel="preload" href="./css/font.css" as="style">
     <link rel="stylesheet" href="./css/font.css">
     <!-- css link -->
     <link rel="preload" href="./css/style.css" as="style">
